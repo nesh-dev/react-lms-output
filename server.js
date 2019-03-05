eecca9251f9bbf36a6231f8a29ad96fcddd6821a
kevin munene mutahi <nesh-dev>
2019-03-05 15:18:08 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static(__dirname + '/client'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.listen(port, () => console.log("Running on localhost:8080"));
+Collapse
+
+
+
