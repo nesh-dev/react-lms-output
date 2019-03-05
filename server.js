b12c1c790c4be8d7c8e134468f1b90b30224c885
kevin munene mutahi <nesh-dev>
2019-03-05 15:54:02 +0300

    fix

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.static(__dirname + '/client'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.sendFile(path.resolve(__dirname, './client/index.html'));
 });
 
+app.listen(port);
 
