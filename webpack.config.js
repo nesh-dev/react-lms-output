357e02f6cf41dc863164364c42c8a8fe5fa37ad3
kevin munene mutahi <nesh-dev>
2019-03-05 21:55:02 +0300

    add public path

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   entry: './client/index.js',
   output: {
     path: path.resolve('dist'),
-    publicPath: '/',
+    publicPath: '/dist/',
     filename: 'index_bundle.js'
   },
   module: {
